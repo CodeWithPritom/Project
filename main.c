9cfb8a71f7105bd5abcf618b33573ed7a863b8ca
Arup Bhowmik Pritom <pritombhowmik360@gmail.com>
2025-08-25 08:10:01 +0000

    I add main and Login functions

--- main.c
+++ main.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Login is Created by CodeWithPritom
+
 int login()
 {
+    printf("Welcome to Mini Terminal OS Simulator\n");
+
     char username[20];
     char password[20];
     char cUser[20] = "Rakib";
@@ -28,25 +32,65 @@ int login()
     
 }
 
+//Member 1 : Logic File Explorer
+
+void file_exp()
+{
+
+}
+
+//Member 2 : Note Pad Integration
+
+void note_pad()
+{
+
+}
+
+//Member 3 : Student Database Integration
+
+void stu_data()
+{
+
+}
+
+
+
+// Main is Create by CodeWithPritom
+
 int main()
 {
-    int choose;
-    if(login())
+    int option = 0;
+
+    if(!login())
     {
         return 0;
     }
-    return 0;
 
     do
     {
-        printf("\n--- File Explorer ---\n");
-        printf("1. Create File\n");
-        printf("2. View File\n");
-        printf("3. Append to File\n");
-        printf("0. Back\n");
+        printf("\n===== Main Menu =====\n");
+        printf("1. File Explorer\n");   // Member 2
+        printf("2. Note Pad\n");    // Member 3
+        printf("3. Student Database\n"); // Member 4
+        printf("0. Exit\n");
         printf("Enter choice: ");
-        scanf("%d", &choice);
-    }
+        scanf("%d", &option);
+
+        switch(option)
+        {
+            case 1 : file_exp();
+            case 2 : note_pad();
+            case 3 : stu_data();
+            default : 
+            if(option < 0 || option > 3)
+            {
+            printf("\nPlease Enter Valid Choice! or ShutDown me!\n");
+            }
+        }
+    }while(option != 0 );
+
+    printf("\nGood Bye Sir... Miss You :')\n");
+    return 0;
 }
 
 
